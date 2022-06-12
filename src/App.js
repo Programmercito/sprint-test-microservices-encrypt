30564781ad56d8ae80103f55b0567d0a77321128
Programmercito <osboceo@live.com>
2022-06-12 10:49:48 -0400

    cambios para enviar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  
+	const [message, setMessage] = useState("");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <form
+			onSubmit={event => {
+			
+			}}
+		>
+			<label>secret message</label>
+			<input type="text" name="message" value={message} />
+
+			<button>Send to channel secure</button>
+		</form>
       </header>
     </div>
   );
