39f3522c8123439ebd61b69b0650fd0d8eacda95
Programmercito <osboceo@live.com>
2022-06-12 12:36:29 -0400

    cambios ya envia la data falta encryptarla

--- src/App.js
+++ src/App.js
@@ -2,24 +2,43 @@ import React, { useState } from 'react'
 import './App.css';
 
 function App() {
-  
-	const [message, setMessage] = useState("");
-  return (
-    <div className="App">
-      <header className="App-header">
-      <form
-			onSubmit={event => {
-			
-			}}
-		>
-			<label>secret message</label>
-			<input type="text" name="message" value={message} />
 
-			<button>Send to channel secure</button>
-		</form>
-      </header>
-    </div>
-  );
+	const [msg, setMsg] = useState("")
+	const handleInputChange = event => {
+		setMsg(event.target.value)
+	}
+	return (
+		<div className="App">
+			<header className="App-header">
+				<form
+					onSubmit={event => {
+						console.log(msg);
+						const requestOptions = {
+							method: 'POST',
+							headers: { 'Content-Type': 'application/json' },
+							body: JSON.stringify({ message: msg })
+						};
+
+						fetch('/api/send', requestOptions)
+							.then(response => response.json())
+							.then((data) => {
+								alert(data.resultado);
+							}, (error) => {
+								alert("Erorr al insertar");
+							});
+
+
+
+					}}
+				>
+					<label>secret message</label>
+					<input type="text" name="message" value={msg} onChange={handleInputChange} />
+
+					<button>Send to channel secure</button>
+				</form>
+			</header>
+		</div>
+	);
 }
 
 export default App;
