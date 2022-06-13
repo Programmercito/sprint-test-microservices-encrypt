445d32db2a7c9b9ceafb841588df5de1a53e4ac0
Programmercito <osboceo@live.com>
2022-06-12 23:19:20 -0400

    cambio de html

--- src/App.js
+++ src/App.js
@@ -10,22 +10,11 @@ function App() {
 	}
 	var encrypt = new JSEncrypt();
 	var publicKey = `
-    ------BEGIN PUBLIC KEY-----
-	MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAu+PDFyKhY1JyJ0X7L1wr
-	p6MNrCcXbP0RtCk/UV+wscIiafHFbK9inwCC7KXd2y+bAKuxmS3Jqsv/QtbHUhPg
-	vi/iWFjBURLW0y1c0JI+O4Vr0OAeT0q/Xtl3MU5QLH1IiZ4CpfMLa7nhcBntJftC
-	mZQ6fK0SIzJuAB75YGFknO6yFcEnHZnTrUGYg3D7vnIWTw0q/CkoejPg3RmI8Hix
-	u/MkbEuRrcc3MKcBcp07ad2KnSNUBT5rVd3ipySrej4zr7kGBMgkNnVJ3cOqeuaU
-	1SDYADUL7RWY3d9XeS8nzgAoM6OP6vzU14zT2Hh0EnFzag2GExdbzXa44fjTveEX
-	0KBNFaRdv9tWbG8Ws0iyUhNea3N4U/bXpYCbl+svTT5LYXRX6c7vb8NTqgpiGDd4
-	7URDhwuVHaSWNshRVS08Tfx40LQdyFV7lEYolgyu0ElF/peVC3XETVZIBCReme21
-	MJ062GE9AAVaN27KdrKtZ8OS7myZuf+kp/z+4K9ZgM6yYHz24VYVuJOlZ+WLzo3o
-	Apf4xhQRwedU+b9i/pbc7ETf5cHLy5x7t7kehlN7hrhwXVuGqeSIQwYe0RBK+lxH
-	b5nMFLrEkH521Xi/UFyF4GZPz0L7qDVNoGYr0Ac0ho+HW62y07B2WsKOvX1z10gS
-	fWAkHMmTPOMTeriWt+wD/IkCAwEAAQ==
+	-----BEGIN PUBLIC KEY-----
+	MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCgFGVfrY4jQSoZQWWygZ83roKXWD4YeT2x2p41dGkPixe73rT2IW04glagN2vgoZoHuOPqa5and6kAmK2ujmCHu6D1auJhE2tXP+yLkpSiYMQucDKmCsWMnW9XlC5K7OSL77TXXcfvTvyZcjObEz6LIBRzs6+FqpFbUO9SJEfh6wIDAQAB
 	-----END PUBLIC KEY-----`;
 	encrypt.setPublicKey(publicKey);
-
+ 
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -54,7 +43,7 @@ function App() {
 					}}
 				>
 					<label>secret message</label>
-					<input type="text"  value={msg} onChange={handleInputChange} />
+					<input type="text" maxlength="117" value={msg} onChange={handleInputChange} />
 
 					<button>Send to channel secure</button>
 				</form>
